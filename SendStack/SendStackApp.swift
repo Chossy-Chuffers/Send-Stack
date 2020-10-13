61492590bb7bd2263dc03dfd859ba3905ff52685
johnwhh <johnwhh@icloud.com>
2020-10-12 23:55:02 -0400

    Initial Commit

---
+++ SendStack/SendStackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SendStackApp.swift
+//  SendStack
+//
+//  Created by John Holtzworth on 10/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SendStackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
